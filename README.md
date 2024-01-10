ff6b68399fdbb696152ae3449982000873405a3a
Pchkauu <pchkauu@gmail.com>
2024-01-10 20:07:57 +0300

    + initial redux level a

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flutter_study
+
+
+# 29 Tevet, 5784
+
++ Redux, Level A
---
+++ state_management/redux/redux_level_a/android/app/src/main/kotlin/io/pchkauu/study/redux_level_a/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.pchkauu.study.redux_level_a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
