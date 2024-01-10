c3db8bf1ee53a01c2601e4fd8e752a14c46a0e73
Pchkauu <pchkauu@gmail.com>
2024-01-10 20:44:17 +0300

    + add increment, decrement, power, reset counter actions

--- state_management/redux/redux_level_a/lib/main.dart
+++ state_management/redux/redux_level_a/lib/main.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: avoid_print
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
 Future<void> main() async {
   await runZonedGuarded(
@@ -10,7 +14,6 @@ Future<void> main() async {
 }
 
 void _onError(Object error, StackTrace stack) {
-  // ignore: avoid_print
   print(
     'Error msg: ${error.toString()};'
     '\n\nStack msg: ${stack.toString()},',
@@ -20,9 +23,62 @@ void _onError(Object error, StackTrace stack) {
 Future<void> _entry() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final Store<int> store = Store(reducer, initialState: 0);
+
   runApp(
-    const CounterApp(),
+    StoreProvider(
+      store: store,
+      child: const CounterApp(),
+    ),
   );
+}
+
+sealed class CounterAction {
+  const CounterAction();
+}
+
+class CounterIncrementAction extends CounterAction {
+  const CounterIncrementAction();
+}
+
+final class CounterDecrementAction extends CounterAction {
+  const CounterDecrementAction();
+}
+
+final class CounterPowerAction extends CounterAction {
+  const CounterPowerAction();
+}
+
+final class CounterResetAction extends CounterAction {
+  const CounterResetAction();
+}
+
+int reducer(int state, dynamic action) {
+  print(
+    action.runtimeType.toString(),
+  );
+
+  if (action is! CounterAction) return state;
+
+  // ignore: unnecessary_cast
+  switch (action) {
+    case CounterIncrementAction():
+      {
+        return ++state;
+      }
+    case CounterDecrementAction():
+      {
+        return --state;
+      }
+    case CounterPowerAction():
+      {
+        return state * state;
+      }
+    case CounterResetAction():
+      {
+        return 0;
+      }
+  }
 }
 
 class CounterApp extends StatelessWidget {
@@ -33,6 +89,7 @@ class CounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: CounterHome(),
     );
   }
@@ -45,9 +102,57 @@ class CounterHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final store = StoreProvider.of<int>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Redux Level A'),
+        title: Text(
+          'Redux Level A',
+          style: theme.textTheme.headlineMedium,
+        ),
+      ),
+      body: Center(
+        child: StoreConnector<int, int>(
+          converter: (store) => store.state,
+          builder: (context, state) {
+            return Text(
+              state.toString(),
+              style: theme.textTheme.displayMedium,
+            );
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              store.dispatch(const CounterPowerAction());
+            },
+            child: const Icon(Icons.upgrade_rounded),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              store.dispatch(const CounterIncrementAction());
+            },
+            child: const Icon(Icons.add_rounded),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              store.dispatch(const CounterDecrementAction());
+            },
+            child: const Icon(Icons.remove_rounded),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              store.dispatch(const CounterResetAction());
+            },
+            child: const Icon(Icons.restore_rounded),
+          ),
+        ],
       ),
     );
   }
