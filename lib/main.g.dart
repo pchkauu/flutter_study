4e3b5837427f9a9f24087056f5ddd07a32e74ba4
Pchkauu <pchkauu@gmail.com>
2024-02-02 15:06:10 +0300

    + copt hello world example

---
+++ lib/main.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$helloWordHash() => r'f1b32316ad569d732b8857b433d6ae9208ed2693';
+
+/// See also [helloWord].
+@ProviderFor(helloWord)
+final helloWordProvider = AutoDisposeProvider<String>.internal(
+  helloWord,
+  name: r'helloWordProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$helloWordHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HelloWordRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
